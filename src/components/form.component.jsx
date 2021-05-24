b37f70a3f449b3a03f5441c02ad24f9cb1c047e1
jimishravat <jimishravat2802@gmail.com>
2021-05-24 18:45:12 +0530

    deploy weather app

--- src/components/form.component.jsx
+++ src/components/form.component.jsx
@@ -4,9 +4,7 @@ import "./form.style.css";
 const Form = (props) => {
   return (
     <div className="container">
-    <div>
-        {props.error ? error() : null}
-    </div>
+      <div>{props.error ? error() : null}</div>
       <form onSubmit={props.loadweather}>
         <div className="row">
           <div className="col-md-3 offset-md-2">
@@ -14,7 +12,6 @@ const Form = (props) => {
               type="text"
               className="form-control"
               name="city"
-              autocomplete="off"
               placeholder="City"
             />
           </div>
@@ -23,7 +20,6 @@ const Form = (props) => {
               type="text"
               className="form-control"
               name="country"
-              autocomplete="off"
               placeholder="Country"
             />
           </div>
@@ -36,12 +32,12 @@ const Form = (props) => {
   );
 };
 
-function error(){
-    return(
-        <div className="alert alert-danger mx-5" role="alert">
-        Please Enter City or Country
-        </div>
-    );
+function error() {
+  return (
+    <div className="alert alert-danger mx-5" role="alert">
+      Please Enter City or Country
+    </div>
+  );
 }
 
 export default Form;
